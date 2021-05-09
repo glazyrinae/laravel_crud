9932bdeae0423cf2f7329f0ceb8f748830a94f18
glazyrinae <railot116@gmail.com>
2021-05-09 10:45:20 +0300

    add temlates and pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        @include('includes.head')
+    </head>
+    <body>
+        <div class="">
+            <header class="row">
+                @include('includes.header')
+            </header>
+            <div id="main" class="container">
+                @yield('content')
+            </div>
+            <footer class="row">
+                @include('includes.footer')
+            </footer>
+        </div>
+    </body>
+</html>
